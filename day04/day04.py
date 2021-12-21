fe844126fba3540ef61b640ba84f27b1e8e2e83a
mikewilzn <mikewilzn@gmail.com>
2021-12-21 18:21:59 -0500

    Day 4 complete

--- day04/day04.py
+++ day04/day04.py
@@ -1,30 +1,47 @@
 from timeit import default_timer
 import numpy as np
 
-# Parses boards input into 3d array
-def setup_boards(boards_array):
-    for board in boards_array:
-        board = board.split('\n')
-    return np.array(boards_array)
+class BingoBoard:
+    def __init__(self, rows):
+        rows = [row.split() for row in rows]
+        self.grid = np.array(rows, dtype=int)
+    
+    def mark_hit(self, num):
+        self.grid[self.grid == num] = -1
+
+    def get_score(self, last_number):
+        return np.sum((self.grid != -1) * self.grid) * last_number
+    
+    def is_winner(self):
+        if any(np.sum(self.grid, axis = 0) == -5):
+            return True
+        elif any(np.sum(self.grid, axis = 1) == -5):
+            return True
+        else:
+            return False
 
 def main():
     start_time = default_timer()
 
-    with open('input.txt') as f:
+    with open('D:\Code Workspaces\AdventofCode2021\day04\input.txt') as f:
         draws = list(map(int, f.readline().split(',')))
-        boards = f.read()[1:].split("\n\n") #[1:] Ignored blank line between draws and the boards
+        boards = f.read()[1:].split('\n\n') #[1:] Ignored blank line between draws and the boards
+
+    boards = [BingoBoard(board.split('\n')) for board in boards]
     
-    boards = setup_boards(boards)
-    print(boards[0][0])
-    """ for num in draws:
-        for board in boards:
-            for row in board:
-                for col in row:
-                    if(int(col) == num):
-                        col = -1 """
+    scores = []
+    for num in draws:
+        # Mark hits on each board
+        [board.mark_hit(num) for board in boards]
+        # Adds winning boards to list of scores
+        scores += [board.get_score(num) for board in boards if board.is_winner()]
+        # Remove winning boards
+        boards = [board for board in boards if not board.is_winner()]
 
     end_time = default_timer()
     
+    print("Part 1: ", scores[0])
+    print("Part 2: ", scores[-1])
     print(f'Time: {1000*(end_time-start_time):.3f}ms')
 
 if __name__ == '__main__':
